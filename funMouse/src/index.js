e9c7148698577d1fdb02fcd4433580c01b297e2f
Cody Goss <c.ward.goss@gmail.com>
2019-03-04 16:42:10 -0500

    init commit

---
+++ funMouse/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function transform(offset) {
+  const cos = Math.cos(offset);
+  const sin = Math.sin(offset);
+  return {
+    transform: `matrix3d(${sin}, ${-cos}, ${sin}, 0, ${-cos}, ${sin}, 0, 0, 0, ${cos}, ${cos}, ${sin}, 0, 0, 0, 1)`
+  };
+}
+
+class App extends React.Component {
+  state = {
+    styleOne: {},
+    styleTwo: {}
+  };
+  onMouseMove = event => {
+    this.setState({
+      styleOne: transform(-event.clientX / event.clientY),
+      styleTwo: transform(event.clientX / event.clientY)
+    });
+  };
+  render() {
+    return (
+      <div onMouseMove={this.onMouseMove}>
+        <div className="panel" style={this.state.styleOne} />
+        <div className="panel" style={this.state.styleTwo} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
